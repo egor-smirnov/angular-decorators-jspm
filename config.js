87961bb8745ca9c7e357368dd82bcafc5bf057da
Egor Smirnov <egorsmir@gmail.com>
2015-06-10 16:36:12 +0200

    add required npm deps via jspm

--- config.js
+++ config.js
@@ -17,17 +17,39 @@ System.config({
   "map": {
     "babel": "npm:babel-core@5.5.6",
     "babel-runtime": "npm:babel-runtime@5.5.6",
+    "clone": "npm:clone@1.0.2",
     "core-js": "npm:core-js@0.9.15",
+    "extend": "npm:extend@2.0.1",
+    "foreach": "npm:foreach@2.0.5",
+    "is-js": "npm:is-js@0.1.1",
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.2.2"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
+    "github:jspm/nodelibs-vm@0.1.0": {
+      "vm-browserify": "npm:vm-browserify@0.0.4"
+    },
     "npm:babel-runtime@5.5.6": {
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:buffer@3.2.2": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "ieee754": "npm:ieee754@1.1.5",
+      "is-array": "npm:is-array@1.0.1"
+    },
+    "npm:clone@1.0.2": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "vm": "github:jspm/nodelibs-vm@0.1.0"
     },
     "npm:core-js@0.9.15": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:vm-browserify@0.0.4": {
+      "indexof": "npm:indexof@0.0.1"
     }
   }
 });
